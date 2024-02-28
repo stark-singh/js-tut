04b9f882067c5729e63a6081a530c59577c22714
stark-singh <52420842+stark-singh@users.noreply.github.com>
2024-02-28 18:38:24 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all JS code as newer version
+
+
+// alert("helo")  we are using node js, not browser
+
+let name = "dherraj"
+let age = 18
+let isLoggIn = false
+
+// nubmer => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / intensionaly abscens of value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
