d7bbd90ec33939b6d80eec18c5f1434acb6f6519
stark-singh <52420842+stark-singh@users.noreply.github.com>
2024-02-28 19:40:18 +0000

    conversion od data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = 25
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLoggIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false 
+// "dheraj" => true
+
+let stringNo = 33
+
+let stringNum = String(stringNo);
+console.log(stringNum);
+console.log(typeof stringNum);
