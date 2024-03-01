2da902f528027f0e77e6bf50bd8b986868527903
stark-singh <52420842+stark-singh@users.noreply.github.com>
2024-03-01 20:21:39 +0000

    nums_maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,43 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.568;
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/* +++++++++++++ Maths ++++++++++++++++++ */
+
+console.log(Math);
+
+console.log(Math.abs(-4));  //convert in positive number..
+console.log(Math.round(5.6));// round off karega value
+console.log(Math.ceil(4.2)); // thoda sa bhi decimal aya to value big no round off hoga
+console.log(Math.floor(4.5)); // thoda sa bhi decimal aya to value small no round off hoga
+console.log(Math.min(4,3,5,6,8)); // min value dikhayga
+console.log(Math.max(4,3,5,6,8)); // max value dikhayga
+
+console.log(Math.random());  // 0-1 k bich mein hi value ayga..
+console.log((Math.random()*10) + 1);  
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+ 
+
+
+
